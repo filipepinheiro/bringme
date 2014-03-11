7fa1750ccde5ca42cbef6bb2c44c4d0439268ceb
Filipe Pinheiro <filipepinheiro@ua.pt>
2014-03-11 19:41:16 +0000

    -Login Screen UI [No funcionality yet!] -Swipe Nav for Main Activity

--- gen/pt/ua/icm/bringme/R.java
+++ gen/pt/ua/icm/bringme/R.java
@@ -9,6 +9,24 @@ package pt.ua.icm.bringme;
 
 public final class R {
     public static final class attr {
+        /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+         */
+        public static final int buttonBarButtonStyle=0x7f010001;
+        /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+         */
+        public static final int buttonBarStyle=0x7f010000;
+    }
+    public static final class color {
+        public static final int black=0x7f040001;
+        public static final int black_overlay=0x7f040000;
+        public static final int blue=0x7f040004;
+        public static final int dark_blue=0x7f040003;
+        public static final int dark_gray=0x7f040007;
+        public static final int gray_cloud=0x7f040006;
+        public static final int green=0x7f040005;
+        public static final int white=0x7f040002;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -17,30 +35,69 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int splashScreenTextSize=0x7f050002;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bring_me_logo=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_forgot_password=0x7f09000a;
+        public static final int action_settings=0x7f09000b;
+        public static final int button1=0x7f090009;
+        public static final int email=0x7f090003;
+        public static final int login=0x7f090005;
+        public static final int login_form=0x7f090001;
+        public static final int login_status=0x7f090000;
+        public static final int logo=0x7f090002;
+        public static final int pager=0x7f090007;
+        public static final int password=0x7f090004;
+        public static final int section_label=0x7f090008;
+        public static final int sign_in_button=0x7f090006;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_login=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int fragment_main_dummy=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int login=0x7f080000;
+        public static final int main=0x7f080001;
+        public static final int test=0x7f080002;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_forgot_password=0x7f06000f;
+        public static final int action_settings=0x7f060001;
+        public static final int action_sign_in_register=0x7f06000d;
+        public static final int action_sign_in_short=0x7f06000e;
+        public static final int app_name=0x7f060000;
+        public static final int dummy_button=0x7f060003;
+        public static final int dummy_content=0x7f060004;
+        public static final int error_field_required=0x7f060014;
+        public static final int error_incorrect_password=0x7f060013;
+        public static final int error_invalid_email=0x7f060011;
+        public static final int error_invalid_password=0x7f060012;
+        public static final int login_progress_signing_in=0x7f060010;
+        /**  Strings related to login 
+         */
+        public static final int prompt_email=0x7f06000b;
+        public static final int prompt_password=0x7f06000c;
+        public static final int title_activity_login=0x7f06000a;
+        public static final int title_activity_main=0x7f060009;
+        public static final int title_activity_splashscreen=0x7f060002;
+        public static final int title_activity_test=0x7f060005;
+        public static final int title_section1=0x7f060006;
+        public static final int title_section2=0x7f060007;
+        public static final int title_section3=0x7f060008;
     }
     public static final class style {
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+
+
     
 
             Theme customizations available in newer API levels can go in
@@ -50,6 +107,7 @@ public final class R {
 
         Base application theme for API 11+. This theme completely replaces
         AppBaseTheme from res/values/styles.xml on API 11+ devices.
+
     
  API 11 theme customizations can go here. 
 
@@ -59,10 +117,57 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+        public static final int ButtonBar=0x7f070003;
+        public static final int ButtonBarButton=0x7f070004;
+        public static final int FullscreenActionBarStyle=0x7f070006;
+        public static final int FullscreenTheme=0x7f070005;
+        public static final int LoginFormContainer=0x7f070002;
     }
+    public static final class styleable {
+        /** 
+         Declare custom theme attributes that allow changing which styles are
+         used for button bars depending on the API level.
+         ?android:attr/buttonBarStyle is new as of API 11 so this is
+         necessary to support previous API levels.
+    
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #ButtonBarContainerTheme_buttonBarButtonStyle pt.ua.icm.bringme:buttonBarButtonStyle}</code></td><td></td></tr>
+           <tr><td><code>{@link #ButtonBarContainerTheme_buttonBarStyle pt.ua.icm.bringme:buttonBarStyle}</code></td><td></td></tr>
+           </table>
+           @see #ButtonBarContainerTheme_buttonBarButtonStyle
+           @see #ButtonBarContainerTheme_buttonBarStyle
+         */
+        public static final int[] ButtonBarContainerTheme = {
+            0x7f010000, 0x7f010001
+        };
+        /**
+          <p>This symbol is the offset where the {@link pt.ua.icm.bringme.R.attr#buttonBarButtonStyle}
+          attribute's value can be found in the {@link #ButtonBarContainerTheme} array.
+
+
+          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+          @attr name pt.ua.icm.bringme:buttonBarButtonStyle
+        */
+        public static final int ButtonBarContainerTheme_buttonBarButtonStyle = 1;
+        /**
+          <p>This symbol is the offset where the {@link pt.ua.icm.bringme.R.attr#buttonBarStyle}
+          attribute's value can be found in the {@link #ButtonBarContainerTheme} array.
+
+
+          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+          @attr name pt.ua.icm.bringme:buttonBarStyle
+        */
+        public static final int ButtonBarContainerTheme_buttonBarStyle = 0;
+    };
 }
